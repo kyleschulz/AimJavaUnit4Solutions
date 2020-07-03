07182b9690b9245bb09454c6aee41115060ebf91
Kyle Schulz <kyle@schulz.com>
2020-07-03 15:18:30 -0500

    add mongodb

--- src/main/java/com/aim/movie/ConnectionFactory.java
+++ src/main/java/com/aim/movie/ConnectionFactory.java
@@ -1,10 +1,13 @@
 package com.aim.movie;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
-
-import org.postgresql.Driver;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class ConnectionFactory {
 
@@ -19,7 +22,50 @@ public class ConnectionFactory {
      */
     public static Connection getConnection() {
         try {
-            DriverManager.registerDriver(new Driver());
+            DriverManager.registerDriver(new Driver() {
+
+                @Override
+                public boolean jdbcCompliant() {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+
+                @Override
+                public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public int getMinorVersion() {
+                    // TODO Auto-generated method stub
+                    return 0;
+                }
+
+                @Override
+                public int getMajorVersion() {
+                    // TODO Auto-generated method stub
+                    return 0;
+                }
+
+                @Override
+                public Connection connect(String url, Properties info) throws SQLException {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public boolean acceptsURL(String url) throws SQLException {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+            });
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (SQLException ex) {
             throw new RuntimeException("Error connecting to the database", ex);
---
+++ src/main/java/com/aim/movie/MongoMovie.java
@@ -0,0 +1,36 @@
+package com.aim.movie;
+
+import java.util.Iterator;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+public class MongoMovie {
+
+    public static void main(String[] args) {
+
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        MongoDatabase database = mongoClient.getDatabase("movie");
+
+        MongoCollection<Document> collection = database.getCollection("actors");
+
+        FindIterable<Document> iterDoc = collection.find();
+        Iterator<Document> it = iterDoc.iterator();
+
+        Actor actor = new Actor();
+        while (it.hasNext()) {
+            Document doc = it.next();
+            actor.setFirstName(doc.getString("firstName"));
+            actor.setLastName(doc.getString("lastName"));
+            System.out.println(doc);
+            System.out.println("Actor: " + actor);
+        }
+
+    }
+
+}
