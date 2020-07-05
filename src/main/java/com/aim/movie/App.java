25ae6a8267c55e05065f7041012bc35fcd9aa8af
Kyle Schulz <kyle@schulz.com>
2020-07-05 17:17:37 -0500

    updates

--- src/main/java/com/aim/movie/App.java
+++ src/main/java/com/aim/movie/App.java
@@ -19,18 +19,22 @@ public class App {
             System.out.println("Connected to PostgreSQL database!");
             Statement statement = connection.createStatement();
             System.out.println("Reading movie records...\n");
-            System.out.format("%-30.30s  %-30.30s%n", "Movie Name", "Director");
-            System.out.format("%-30.30s  %-30.30s%n", "----------", "--------");
+            System.out.format("%-35s %-5s %-30s%n", "Movie Name", "Len", "Director");
+            System.out.format("%-35s %-5s %-30s%n", "----------", "---", "--------");
 
             ResultSet resultSet = statement.executeQuery(
-                    "SELECT m.movie_name, concat(d.first_name, ' ', d.last_name) full_name FROM movies m JOIN directors d on m.director_id = d.director_id;");
+                    "SELECT m.movie_name, m.movie_length, concat(d.first_name, ' ', d.last_name) full_name FROM movies m JOIN directors d on m.director_id = d.director_id;");
             while (resultSet.next()) {
-                System.out.format("%-30.30s  %-30.30s%n", resultSet.getString("movie_name"),
-                        resultSet.getString("full_name"));
+                // read data from each row
+                String movieName = resultSet.getString("movie_name");
+                String fullName = resultSet.getString("full_name");
+                int movieLength = resultSet.getInt(2);
+                System.out.format("%-35s %-5d %-30s%n", movieName, movieLength, fullName);
             }
 
         } catch (SQLException e) {
             System.out.println("Connection failure.");
+            e.printStackTrace();
         } finally {
             System.out.println("\nGoodbye ...");
         }
